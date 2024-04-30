c9fe8c75e17235e0c956b52d8f18080fad8f558d
behnamZM <behnamzare100@yahoo.com>
2024-04-30 07:23:22 -0700

    show user infos on header

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 export default function Navbar() {
+  const { currentUser } = useSelector((state) => state.user);
   const path = useLocation().pathname;
+  console.log(currentUser);
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -56,37 +59,46 @@ export default function Navbar() {
             className="input input-bordered w-24 md:w-auto"
           />
         </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+        {currentUser ? (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <span className="text-center mb-3">
+                خوش اومدی {currentUser.username} عزیز :)
+              </span>
+              <li>
+                <Link to={"/dashboard?tab=profile"} className="justify-between">
+                  پروفایل
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>تنظیمات</a>
+              </li>
+              <li>
+                <a>خروج</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        ) : (
+          <Link className="btn btn-primary btn-outline" to="signin">
+            ورود
+          </Link>
+        )}
       </div>
     </div>
   );
